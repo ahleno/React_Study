2163107eaefae19601d477a800c6e724bb093ea9
Han eol Kim <yamae3203@gmail.com>
2023-05-11 10:18:44 +0900

    2

--- src/Hansot.js
+++ src/Hansot.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header.js";
-import SideBar from "./components/SideBar";
+import SideBar from "./components/SideBar.js";
+import MenuRow from "./components/MenuRow.js";
 
 function Hansot() {
   return (
@@ -8,35 +9,7 @@ function Hansot() {
       <div className="content-container">
         <SideBar></SideBar>
         <div className="menu-container">
-          <div className="menu-container-row">
-            <div className="menu-card">
-              <div className="menu-img">
-                <img src="./img/menu1.jpg" alt="1993 왕돈까스 도시락" />
-              </div>
-              <div class="menu-text">
-                <h4>나시고랭 콤보</h4>
-                <h4>6,800</h4>
-              </div>
-            </div>
-            <div className="menu-card">
-              <div className="menu-img">
-                <img src="./img/menu2.jpg" alt="1993 왕돈까스 도시락" />
-              </div>
-              <div class="menu-text">
-                <h4>나시고랭 콤보</h4>
-                <h4>6,800</h4>
-              </div>
-            </div>
-            <div className="menu-card">
-              <div className="menu-img">
-                <img src="./img/menu3.jpg" alt="1993 왕돈까스 도시락" />
-              </div>
-              <div class="menu-text">
-                <h4>나시고랭 콤보</h4>
-                <h4>6,800</h4>
-              </div>
-            </div>
-          </div>
+            <MenuRow></MenuRow>
         </div>
       </div>
     </div>
---
+++ src/components/MenuRow.js
@@ -0,0 +1,35 @@
+function MenuRow() {
+    return (
+        <div className="menu-container-row">
+        <div className="menu-card">
+          <div className="menu-img">
+            <img src="./img/menu1.jpg" alt="1993 왕돈까스 도시락" />
+          </div>
+          <div class="menu-text">
+            <h4>나시고랭 콤보</h4>
+            <h4>6,800</h4>
+          </div>
+        </div>
+        <div className="menu-card">
+          <div className="menu-img">
+            <img src="./img/menu2.jpg" alt="1993 왕돈까스 도시락" />
+          </div>
+          <div class="menu-text">
+            <h4>나시고랭 콤보</h4>
+            <h4>6,800</h4>
+          </div>
+        </div>
+        <div className="menu-card">
+          <div className="menu-img">
+            <img src="./img/menu3.jpg" alt="1993 왕돈까스 도시락" />
+          </div>
+          <div class="menu-text">
+            <h4>나시고랭 콤보</h4>
+            <h4>6,800</h4>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default MenuRow;
